a91355cb47561dfcdb8ec04cf19301f3d7cf6f3f
Valentin Meica <valentinmeica@gmail.com>
2019-01-23 20:45:25 -0500

    upload data from csv file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,11 +4,11 @@ const models = require('../models');
 //restAPI
 module.exports = function (app) {
   
-  const product = new RestfulAPI('products', app, models.Product);
-  product.findAll();
-  product.create();
-  product.delete('id')
-  product.update('id')
+  const black_List = new RestfulAPI('Black_Lists', app, models.Black_List);
+  black_List.findAll();
+  black_List.create();
+  black_List.delete('id')
+  black_List.update('id')
 
   /* const user = new RestfulAPI('user', app, models.User);
   user.find('id');
@@ -27,10 +27,10 @@ module.exports = function (app) {
 
 //require models library
 /* const db = require('../models');
- module.exports = function (app) {
+module.exports = function (app) {
 
     // GET products
-    app.get('/api/products', function (req, res) {
+    app.get('/api/Black_Lists', function (req, res) {
         db.Product.findAll({}).then(function (rows) {
             res.json(rows);
         }).catch(function (error) {
@@ -38,7 +38,7 @@ module.exports = function (app) {
         });
     });
     //GET the products by id
-    app.get('/api/products/:id', function (req, res) {
+    app.get('/api/Black_Lists/:id', function (req, res) {
         db.Product.find({ where: { id: req.params.id } })
             .then(function (data) {
                 res.json(data);
@@ -47,7 +47,7 @@ module.exports = function (app) {
             });
     });
     //Update by id
-    app.put('/api/products/:id', function (req, res) {
+    app.put('/api/Black_Lists/:id', function (req, res) {
         console.log(req.params.id, "we are in update");
         db.Product.update(
             req.body,
@@ -58,4 +58,15 @@ module.exports = function (app) {
             res.json({ success: false, error: error });
         });
     });
-};  */
+
+    app.post('/api/Black_Lists', function (req, res) {
+        models.Task.create(req.body)
+            .then(function (data) {
+                res.json(data);
+            })
+            .err(function (err) {
+                res.json(err);
+            })
+    });
+
+};   */
--- server.js
+++ server.js
@@ -31,3 +31,56 @@ db.sequelize.sync().then(function () {
 app.listen(PORT, function () {
     console.log(`App is now listening on PORT ${PORT}`)
 });
+
+
+/* var request = require("request"),
+    csv = require("csv");
+
+module.exports = function(app) {
+    // accepts the POST form submit of the CSV file
+    app.post("/api/black_List", function(req, res) {
+        // the name under "files" must correspond to the name of the
+        // file input field in the submitted form (here: "csvdata")
+        csv().from.path(req.files.csvdata.path, {
+            delimiter: ",",
+            escape: '"'
+        })
+        // when a record is found in the CSV file (a row)
+        .on("record", function(row, index) {
+            var firstName, lastName;
+            var time, sso, action, device_Id, count, message;
+
+            // skip the header row
+            if (index === 0) {
+                return;
+            }
+
+            // read in the data from the row
+            //firstName = row[0].trim();
+            //lastName = row[1].trim();
+            time = row[0].trim();
+            sso = row[1].trim();
+            action = row[2].trim();
+            device_Id = row[3].trim();
+            count = row[4].trim();
+            message = row[5].trim();
+
+            // perform some operation with the data
+            // ...
+        })
+        // when the end of the CSV document is reached
+        .on("end", function() {
+            // redirect back to the root
+            res.redirect("/");
+        })
+        // if any errors occur
+        .on("error", function(error) {
+            console.log(error.message);
+        });
+    });
+}; */
+
+
+
+
+
